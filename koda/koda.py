16f9fe12f6c2ae49d98ea23e0f31d3b4e7866b0b
DocChinchillin <docchinchillin@gmail.com>
2021-04-05 13:17:26 +0200

    Dodana vizualizacija, popravek data array
    train data ne vsebuje več odgovorov/ rezultatov, to je v ločenem np.array rezultati, vsi data array so zdaj np.array,
    
    Vizualizacija vsebuje histogram rezultatov in slike prvih 8ih številk, katero številko želimo podamo kot argument funkcije

--- koda/koda.py
+++ koda/koda.py
@@ -1,21 +1,40 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
-train_raw = np.genfromtxt('../podatki/train.csv', delimiter=',', skip_header=1)
-test_raw = np.genfromtxt('../podatki/test.csv', delimiter=',', skip_header=1)
-
+print("Getting data")
+train_raw = np.genfromtxt('../podatki/train.csv', delimiter=',', skip_header=1, dtype=np.int16)
+test_raw = np.genfromtxt('../podatki/test.csv', delimiter=',', skip_header=1, dtype=np.int16)
+print("Got data")
 train_data = []
 test_data = []
-
+rezultati = []
 #razdelitev podatkov v terke z dejanskim rezultatom in matriko (tabelo tabel) velikosti 28x28
 for line in train_raw:
-    train_data.append((line[1], line[1:].reshape(28, 28)))
+    rezultati.append(line[0])
+    train_data.append(line[1:].reshape(28, 28))
 for line in test_raw:
     test_data.append(line.reshape(28, 28))
 
+train_data = np.array(train_data)
+test_data = np.array(test_data)
+rezultati = np.array(rezultati)   #pricakovani rezultati...del train_data
 
-#samo za demo
-plt.imshow(train_data[0][1])
-plt.show()
-plt.imshow(test_data[0])
-plt.show()
+
+def vizualizacija(stevilka):
+    fig, axs = plt.subplots(3, 3)
+    axs[0,0].hist(rezultati, density=True, bins=10, edgecolor='black', linewidth=1.2)
+    selection = rezultati == stevilka
+    selected_data = train_data[selection]
+    axs[0,1].imshow(selected_data[0])
+    axs[0,2].imshow(selected_data[1])
+    axs[1,0].imshow(selected_data[2])
+    axs[1,1].imshow(selected_data[3])
+    axs[1,2].imshow(selected_data[4])
+    axs[2,0].imshow(selected_data[5])
+    axs[2,1].imshow(selected_data[6])
+    axs[2,2].imshow(selected_data[7])
+
+
+    plt.show()
+
+#Vpiši vizualizacija katere številke te zanima, dobis prvih 8
+vizualizacija(1)
