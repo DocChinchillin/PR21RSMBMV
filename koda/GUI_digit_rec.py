b740e23298963971f7de7cb0ef47c82dc3cffc96
DocChinchillin <docchinchillin@gmail.com>
2021-05-13 17:45:28 +0200

    GUI implemented
    GUI dodan...ko narises in stisnes save ti naredi/overwrite new_digit.png ki je velikosti 224*224

--- koda/GUI_digit_rec.py
+++ koda/GUI_digit_rec.py
@@ -1,31 +1,58 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt, QFile,QDataStream
+from PyQt5.QtGui import QPen
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+                             QHBoxLayout, QVBoxLayout, QApplication)
 
-from PyQt5 import QtWidgets, QtGui, QtCore
 
+class MainWindow(QtWidgets.QMainWindow):
+    def saveToFile(self):
 
-class App(QtWidgets.QMainWindow):
+        print(self.label.pixmap().save("new_digit.png","png"))
+
     def __init__(self):
         super().__init__()
 
         self.label = QtWidgets.QLabel()
-        canvas = QtGui.QPixmap(600, 600)
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+
+        self.canvas = QtGui.QPixmap(224, 224)  # velikost canvasa za risat 28 *2^3
+        self.canvas.fill(QtGui.QColor("white"))  # zacetna barva canvasa
+        self.setFixedSize(224, 244)  # velikost windowa
+
+        self.savebutton = QtWidgets.QPushButton(self)
+        self.savebutton.setText("Save")
+        self.savebutton.clicked.connect(self.saveToFile)  # save to file event
+        self.label.setPixmap(self.canvas)
+
+
+        w = QtWidgets.QWidget()
+        l = QtWidgets.QVBoxLayout()
+        w.setLayout(l)
+        l.addWidget(self.label)
+        l.addWidget(self.savebutton)
+
+        self.setCentralWidget(w)
+
         self.last_x, self.last_y = None, None
 
-
     def mouseMoveEvent(self, e):
+        napaka = -10
         if self.last_x is None: # First event.
-            self.last_x = e.x()
+            self.last_x = e.x() + napaka
             self.last_y = e.y()
             return # Ignore the first time.
 
         painter = QtGui.QPainter(self.label.pixmap())
-        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        pen = QPen(Qt.black)
+        pen.setWidth(8)
+        painter.setPen(pen)
+        painter.drawLine(self.last_x, self.last_y, e.x()+napaka, e.y())
         painter.end()
         self.update()
 
         # Update the origin for next time.
-        self.last_x = e.x()
+        self.last_x = e.x()+napaka
         self.last_y = e.y()
 
     def mouseReleaseEvent(self, e):
@@ -33,7 +60,7 @@ class App(QtWidgets.QMainWindow):
         self.last_y = None
 
 
-app = QtWidgets.QApplication([])
-widget = App()
-widget.show()
-app.exec()
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
